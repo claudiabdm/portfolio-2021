50cb1d0407080e7aa789f3912c0c604a6c43d8c7
claudiabdm <claudiabdm@hotmail.es>
2021-06-24 22:55:35 +0200

    Fix test

--- test/MyButton.spec.js
+++ test/MyButton.spec.js
@@ -1,9 +1,9 @@
 import { mount } from '@vue/test-utils';
-import Logo from '@/components/Logo.vue';
+import MyButton from '@/components/MyButton.vue';
 
-describe('Logo', () => {
+describe('MyButton', () => {
   test('is a Vue instance', () => {
-    const wrapper = mount(Logo);
+    const wrapper = mount(MyButton);
     expect(wrapper.vm).toBeTruthy();
   });
 });
