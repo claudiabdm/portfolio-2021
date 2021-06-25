055f3ea7743210ce7f81d916d1e75e53306359c9
claudiabdm <claudiabdm@hotmail.es>
2021-06-25 16:22:21 +0200

    Fix storybridge typescript error
    Signed-off-by: claudiabdm <claudiabdm@hotmail.es>

--- pages/index.vue
+++ pages/index.vue
@@ -9,47 +9,65 @@
   </section>
 </template>
 
-<script lang="js">
-export default {
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
   name: 'Home',
-  asyncData (context) {
-    const version = context.query._storyblok || context.isDev ? 'draft' : 'published';
-    const fullSlug = (context.route.path === '/' || context.route.path === '') ? 'home' : context.route.path;
-    return context.app.$storyapi.get(`cdn/stories/${fullSlug}`, {
-      version
-    }).then((res) => {
-      return res.data
-    }).catch((res) => {
-      if (!res.response) {
-        context.error({ statusCode: 404, message: 'Failed to receive content form api' })
-      } else {
-        context.error({ statusCode: res.response.status, message: res.response.data })
-      }
-    })
+  asyncData(context) {
+    const version =
+      context.query._storyblok || context.isDev ? 'draft' : 'published';
+    const fullSlug =
+      context.route.path === '/' || context.route.path === ''
+        ? 'home'
+        : context.route.path;
+    return context.app.$storyapi
+      .get(`cdn/stories/${fullSlug}`, {
+        version,
+      })
+      .then((res: { data: any }) => {
+        return res.data;
+      })
+      .catch((res: { response: { status: any; data: any } }): void => {
+        if (!res.response) {
+          context.error({
+            statusCode: 404,
+            message: 'Failed to receive content form api',
+          });
+        } else {
+          context.error({
+            statusCode: res.response.status,
+            message: res.response.data,
+          });
+        }
+      });
   },
   data() {
     return {
       story: {
+        id: '',
         content: {},
       },
     };
   },
   mounted() {
-    this.$storybridge(
-      () => {
-        const storyblokInstance = new StoryblokBridge();
+    this.$nuxt.$storybridge(() => {
+      const { StoryblokBridge } = window as any;
+      const storyblokInstance = new StoryblokBridge();
 
-        storyblokInstance.on(['input', 'published', 'change'], (event) => {
-        if (event.action === 'input') {
+      storyblokInstance.on(
+        ['input', 'published', 'change'],
+        (event: StoryblokEventPayload) => {
+          if (event.action === 'input') {
             if (event.story.id === this.story.id) {
               this.story.content = event.story.content;
             }
           } else {
             window.location.reload();
           }
-        });
-      },
-    );
-  }
-};
+        }
+      );
+    });
+  },
+});
 </script>
