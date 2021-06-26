245232b5e94439a57f18d35db8859b4527bd7895
claudiabdm <claudiabdm@hotmail.es>
2021-06-26 21:58:00 +0200

    Add light/dark schemes
    Signed-off-by: claudiabdm <claudiabdm@hotmail.es>

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,12 +1,13 @@
 <template>
   <div>
+    <TheHeader />
     <Nuxt />
   </div>
 </template>
-
 <style lang="scss">
 html {
   font-family: var(--font-family-secondary);
   font-size: 16px;
+  background-color: var(--background);
 }
 </style>
