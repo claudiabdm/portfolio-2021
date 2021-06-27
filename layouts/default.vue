e7f461b0965320290bcb5c9fb792b43b3c9068ce
claudiabdm <claudiabdm@hotmail.es>
2021-06-27 23:17:15 +0200

    Add background transition
    Signed-off-by: claudiabdm <claudiabdm@hotmail.es>

--- layouts/default.vue
+++ layouts/default.vue
@@ -8,6 +8,9 @@
 html {
   font-family: var(--font-family-secondary);
   font-size: 16px;
+}
+body {
   background-color: var(--background);
+  transition: background-color 0.15s linear;
 }
 </style>
