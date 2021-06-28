d9d2c22cd0b6b2448b514a81460112bcebb16b2f
claudiabdm <claudiabdm@hotmail.es>
2021-06-28 22:48:03 +0200

    Add hero component (only mobile view)
    Signed-off-by: claudiabdm <claudiabdm@hotmail.es>

---
+++ components/MyHero.vue
@@ -0,0 +1,47 @@
+<template>
+  <section class="hero">
+    <MyRichText class="hero__text" :blok="blok.text" />
+    <MyButtonList class="hero__buttons" :buttons="blok.buttonList[0].buttons" />
+    <MyHeroFigure class="hero__figure" />
+  </section>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  props: {
+    blok: {
+      type: Object,
+      default: () => {},
+    },
+  },
+});
+</script>
+
+<style scoped lang="scss">
+@use '~/assets/styles/global/variables' as *;
+@use '~/assets/styles/mixins/mixins' as *;
+
+.hero {
+  @include flex(flex-start, center, column);
+  @include size(100%, 100%);
+  height: calc(100vh - (#{$header-height} + #{$nav-height} + 20px));
+  color: var(--primary);
+
+  @media screen and (min-width: 1024px) {
+    flex-direction: row;
+  }
+  &__text {
+    font-family: var(--font-family);
+    padding: 0 rem(25px);
+  }
+  &__buttons {
+    display: none;
+  }
+  &__figure {
+    @include size(100%, 100%);
+    align-self: center;
+  }
+}
+</style>
