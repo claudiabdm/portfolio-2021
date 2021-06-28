d446eed794e810530386312968b3f9076873c6d7
claudiabdm <claudiabdm@hotmail.es>
2021-06-28 22:46:35 +0200

    Add button list component
    Signed-off-by: claudiabdm <claudiabdm@hotmail.es>

---
+++ components/MyButtonList.vue
@@ -0,0 +1,23 @@
+<template>
+  <ul class="button-list">
+    <li v-for="button in buttons" :key="button._uid" class="button-list__elem">
+      <MyButton :blok="button" />
+    </li>
+  </ul>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import { Button } from '~/types/components';
+
+export default Vue.extend({
+  props: {
+    buttons: {
+      type: Array as () => Button[],
+      default: () => [],
+    },
+  },
+});
+</script>
+
+<style scoped></style>
