f559afac0f040319bf35da2664b8443a4674e0ff
claudiabdm <claudiabdm@hotmail.es>
2021-06-29 23:22:47 +0200

    Add gsap animation
    Signed-off-by: claudiabdm <claudiabdm@hotmail.es>

--- components/SvgPoints.vue
+++ components/SvgPoints.vue
@@ -5,42 +5,284 @@
       viewBox="0 0 200 200"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
+      @mousemove="onHover"
     >
-      <ellipse cx="16.171" cy="16.17" rx="8" ry="8" fill="currentColor" />
-      <ellipse cx="58.086" cy="16.17" rx="8" ry="8" fill="currentColor" />
-      <ellipse cx="100" cy="16.17" rx="8" ry="8" fill="currentColor" />
-      <ellipse cx="141.914" cy="16.17" rx="8" ry="8" fill="currentColor" />
-      <ellipse cx="183.829" cy="16.17" rx="8" ry="8" fill="currentColor" />
-      <ellipse cx="16.171" cy="58.085" rx="8" ry="8" fill="currentColor" />
-      <ellipse cx="58.086" cy="58.085" rx="8" ry="8" fill="currentColor" />
-      <ellipse cx="100" cy="58.085" rx="8" ry="8" fill="currentColor" />
-      <ellipse cx="141.914" cy="58.085" rx="8" ry="8" fill="currentColor" />
-      <ellipse cx="183.829" cy="58.085" rx="8" ry="8" fill="currentColor" />
-      <ellipse cx="16.171" cy="100" rx="8" ry="8" fill="currentColor" />
-      <ellipse cx="58.086" cy="100" rx="8" ry="8" fill="currentColor" />
-      <ellipse cx="100" cy="100" rx="8" ry="8" fill="currentColor" />
-      <ellipse cx="141.914" cy="100" rx="8" ry="8" fill="currentColor" />
-      <ellipse cx="183.829" cy="100" rx="8" ry="8" fill="currentColor" />
-      <ellipse cx="16.171" cy="141.915" rx="8" ry="8" fill="currentColor" />
-      <ellipse cx="58.086" cy="141.915" rx="8" ry="8" fill="currentColor" />
-      <ellipse cx="100" cy="141.915" rx="8" ry="8" fill="currentColor" />
-      <ellipse cx="141.914" cy="141.915" rx="8" ry="8" fill="currentColor" />
-      <ellipse cx="183.829" cy="141.915" rx="8" ry="8" fill="currentColor" />
-      <ellipse cx="16.171" cy="183.83" rx="8" ry="8" fill="currentColor" />
-      <ellipse cx="58.086" cy="183.83" rx="8" ry="8" fill="currentColor" />
-      <ellipse cx="100" cy="183.83" rx="8" ry="8" fill="currentColor" />
-      <ellipse cx="141.914" cy="183.83" rx="8" ry="8" fill="currentColor" />
-      <ellipse cx="183.829" cy="183.83" rx="8" ry="8" fill="currentColor" />
+      <ellipse cx="16.171" cy="16.17" rx="20" ry="20" fill="transparent" />
+      <ellipse
+        class="ellipse"
+        cx="16.171"
+        cy="16.17"
+        rx="8"
+        ry="8"
+        fill="currentColor"
+      />
+      <ellipse cx="58.086" cy="16.17" rx="20" ry="20" fill="transparent" />
+      <ellipse
+        class="ellipse"
+        cx="58.086"
+        cy="16.17"
+        rx="8"
+        ry="8"
+        fill="currentColor"
+      />
+      <ellipse cx="100" cy="16.17" rx="20" ry="20" fill="transparent" />
+      <ellipse
+        class="ellipse"
+        cx="100"
+        cy="16.17"
+        rx="8"
+        ry="8"
+        fill="currentColor"
+      />
+      <ellipse cx="141.914" cy="16.17" rx="20" ry="20" fill="transparent" />
+      <ellipse
+        class="ellipse"
+        cx="141.914"
+        cy="16.17"
+        rx="8"
+        ry="8"
+        fill="currentColor"
+      />
+      <ellipse cx="183.829" cy="16.17" rx="20" ry="20" fill="transparent" />
+      <ellipse
+        class="ellipse"
+        cx="183.829"
+        cy="16.17"
+        rx="8"
+        ry="8"
+        fill="currentColor"
+      />
+      <ellipse cx="16.171" cy="58.085" rx="20" ry="20" fill="transparent" />
+      <ellipse
+        class="ellipse"
+        cx="16.171"
+        cy="58.085"
+        rx="8"
+        ry="8"
+        fill="currentColor"
+      />
+      <ellipse cx="58.086" cy="58.085" rx="20" ry="20" fill="transparent" />
+      <ellipse
+        class="ellipse"
+        cx="58.086"
+        cy="58.085"
+        rx="8"
+        ry="8"
+        fill="currentColor"
+      />
+      <ellipse cx="100" cy="58.085" rx="20" ry="20" fill="transparent" />
+      <ellipse
+        class="ellipse"
+        cx="100"
+        cy="58.085"
+        rx="8"
+        ry="8"
+        fill="currentColor"
+      />
+      <ellipse cx="141.914" cy="58.085" rx="20" ry="20" fill="transparent" />
+      <ellipse
+        class="ellipse"
+        cx="141.914"
+        cy="58.085"
+        rx="8"
+        ry="8"
+        fill="currentColor"
+      />
+      <ellipse cx="183.829" cy="58.085" rx="20" ry="20" fill="transparent" />
+      <ellipse
+        class="ellipse"
+        cx="183.829"
+        cy="58.085"
+        rx="8"
+        ry="8"
+        fill="currentColor"
+      />
+      <ellipse cx="16.171" cy="100" rx="20" ry="20" fill="transparent" />
+      <ellipse
+        class="ellipse"
+        cx="16.171"
+        cy="100"
+        rx="8"
+        ry="8"
+        fill="currentColor"
+      />
+      <ellipse cx="58.086" cy="100" rx="20" ry="20" fill="transparent" />
+      <ellipse
+        class="ellipse"
+        cx="58.086"
+        cy="100"
+        rx="8"
+        ry="8"
+        fill="currentColor"
+      />
+      <ellipse cx="100" cy="100" rx="20" ry="20" fill="transparent" />
+      <ellipse
+        class="ellipse"
+        cx="100"
+        cy="100"
+        rx="8"
+        ry="8"
+        fill="currentColor"
+      />
+      <ellipse cx="141.914" cy="100" rx="20" ry="20" fill="transparent" />
+      <ellipse
+        class="ellipse"
+        cx="141.914"
+        cy="100"
+        rx="8"
+        ry="8"
+        fill="currentColor"
+      />
+      <ellipse cx="183.829" cy="100" rx="20" ry="20" fill="transparent" />
+      <ellipse
+        class="ellipse"
+        cx="183.829"
+        cy="100"
+        rx="8"
+        ry="8"
+        fill="currentColor"
+      />
+      <ellipse cx="16.171" cy="141.915" rx="20" ry="20" fill="transparent" />
+      <ellipse
+        class="ellipse"
+        cx="16.171"
+        cy="141.915"
+        rx="8"
+        ry="8"
+        fill="currentColor"
+      />
+      <ellipse cx="58.086" cy="141.915" rx="20" ry="20" fill="transparent" />
+      <ellipse
+        class="ellipse"
+        cx="58.086"
+        cy="141.915"
+        rx="8"
+        ry="8"
+        fill="currentColor"
+      />
+      <ellipse cx="100" cy="141.915" rx="20" ry="20" fill="transparent" />
+      <ellipse
+        class="ellipse"
+        cx="100"
+        cy="141.915"
+        rx="8"
+        ry="8"
+        fill="currentColor"
+      />
+      <ellipse cx="141.914" cy="141.915" rx="20" ry="20" fill="transparent" />
+      <ellipse
+        class="ellipse"
+        cx="141.914"
+        cy="141.915"
+        rx="8"
+        ry="8"
+        fill="currentColor"
+      />
+      <ellipse cx="183.829" cy="141.915" rx="20" ry="20" fill="transparent" />
+      <ellipse
+        class="ellipse"
+        cx="183.829"
+        cy="141.915"
+        rx="8"
+        ry="8"
+        fill="currentColor"
+      />
+      <ellipse cx="16.171" cy="183.83" rx="20" ry="20" fill="transparent" />
+      <ellipse
+        class="ellipse"
+        cx="16.171"
+        cy="183.83"
+        rx="8"
+        ry="8"
+        fill="currentColor"
+      />
+      <ellipse cx="58.086" cy="183.83" rx="20" ry="20" fill="transparent" />
+      <ellipse
+        class="ellipse"
+        cx="58.086"
+        cy="183.83"
+        rx="8"
+        ry="8"
+        fill="currentColor"
+      />
+      <ellipse cx="100" cy="183.83" rx="20" ry="20" fill="transparent" />
+      <ellipse
+        class="ellipse"
+        cx="100"
+        cy="183.83"
+        rx="8"
+        ry="8"
+        fill="currentColor"
+      />
+      <ellipse cx="141.914" cy="183.83" rx="20" ry="20" fill="transparent" />
+      <ellipse
+        class="ellipse"
+        cx="141.914"
+        cy="183.83"
+        rx="8"
+        ry="8"
+        fill="currentColor"
+      />
+      <ellipse
+        class="ellipse"
+        cx="183.829"
+        cy="183.83"
+        rx="8"
+        ry="8"
+        fill="currentColor"
+      />
     </svg>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
+import gsap from 'gsap';
 
 export default Vue.extend({
   name: 'SvgPoints',
+  mounted() {
+    gsap.from('#points ellipse', {
+      duration: 2,
+      scale: 0.1,
+      y: 40,
+      ease: 'bounce',
+      stagger: {
+        grid: [5, 5],
+        from: 'edges',
+        amount: 1.5,
+      },
+    });
+  },
+  methods: {
+    onHover(e: MouseEvent): void {
+      const elem = e.target as Element;
+      if (elem.tagName === 'ellipse') {
+        const x = elem.getAttribute('cx');
+        const y = elem.getAttribute('cy');
+        gsap.fromTo(
+          `.ellipse[cx="${x}"][cy="${y}"]`,
+          {
+            scale: 0.25,
+            transformOrigin: 'center',
+          },
+          {
+            duration: 1.5,
+            scale: 1,
+            ease: 'elastic.inOut(1, 0.3)',
+          }
+        );
+      }
+    },
+  },
 });
 </script>
 
-<style scoped></style>
+<style lang="scss" scoped>
+svg {
+  pointer-events: none;
+  ellipse {
+    pointer-events: all;
+  }
+}
+</style>
