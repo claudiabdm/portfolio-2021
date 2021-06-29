566755853f96df5e43b578a337dd06c3f784fbc6
claudiabdm <claudiabdm@hotmail.es>
2021-06-29 21:52:14 +0200

    Create svg vue components
    Signed-off-by: claudiabdm <claudiabdm@hotmail.es>

---
+++ components/SvgPoints.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <svg
+      id="points"
+      viewBox="0 0 200 200"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <ellipse cx="16.171" cy="16.17" rx="8" ry="8" fill="currentColor" />
+      <ellipse cx="58.086" cy="16.17" rx="8" ry="8" fill="currentColor" />
+      <ellipse cx="100" cy="16.17" rx="8" ry="8" fill="currentColor" />
+      <ellipse cx="141.914" cy="16.17" rx="8" ry="8" fill="currentColor" />
+      <ellipse cx="183.829" cy="16.17" rx="8" ry="8" fill="currentColor" />
+      <ellipse cx="16.171" cy="58.085" rx="8" ry="8" fill="currentColor" />
+      <ellipse cx="58.086" cy="58.085" rx="8" ry="8" fill="currentColor" />
+      <ellipse cx="100" cy="58.085" rx="8" ry="8" fill="currentColor" />
+      <ellipse cx="141.914" cy="58.085" rx="8" ry="8" fill="currentColor" />
+      <ellipse cx="183.829" cy="58.085" rx="8" ry="8" fill="currentColor" />
+      <ellipse cx="16.171" cy="100" rx="8" ry="8" fill="currentColor" />
+      <ellipse cx="58.086" cy="100" rx="8" ry="8" fill="currentColor" />
+      <ellipse cx="100" cy="100" rx="8" ry="8" fill="currentColor" />
+      <ellipse cx="141.914" cy="100" rx="8" ry="8" fill="currentColor" />
+      <ellipse cx="183.829" cy="100" rx="8" ry="8" fill="currentColor" />
+      <ellipse cx="16.171" cy="141.915" rx="8" ry="8" fill="currentColor" />
+      <ellipse cx="58.086" cy="141.915" rx="8" ry="8" fill="currentColor" />
+      <ellipse cx="100" cy="141.915" rx="8" ry="8" fill="currentColor" />
+      <ellipse cx="141.914" cy="141.915" rx="8" ry="8" fill="currentColor" />
+      <ellipse cx="183.829" cy="141.915" rx="8" ry="8" fill="currentColor" />
+      <ellipse cx="16.171" cy="183.83" rx="8" ry="8" fill="currentColor" />
+      <ellipse cx="58.086" cy="183.83" rx="8" ry="8" fill="currentColor" />
+      <ellipse cx="100" cy="183.83" rx="8" ry="8" fill="currentColor" />
+      <ellipse cx="141.914" cy="183.83" rx="8" ry="8" fill="currentColor" />
+      <ellipse cx="183.829" cy="183.83" rx="8" ry="8" fill="currentColor" />
+    </svg>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  name: 'SvgPoints',
+});
+</script>
+
+<style scoped></style>
