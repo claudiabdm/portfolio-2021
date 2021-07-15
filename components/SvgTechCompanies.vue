40eff1fcb6264204cf75160e4a75aeed7e47265c
claudiabdm <claudiabdm@hotmail.es>
2021-07-15 20:05:48 +0200

    Add tech companies animated svg
    Signed-off-by: claudiabdm <claudiabdm@hotmail.es>

---
+++ components/SvgTechCompanies.vue
@@ -0,0 +1,203 @@
+<template>
+  <svg
+    class="svg"
+    xmlns="http://www.w3.org/2000/svg"
+    xmlns:xlink="http://www.w3.org/1999/xlink"
+    viewBox="0 0 64 64"
+  >
+    <title>Software Compenies</title>
+    <g id="Software_Compenies" data-name="Software Compenies">
+      <path class="stroke cls-1" d="M63,52V1H1V52Z" />
+      <rect class="cls-2" x="1.25" y="50.75" width="61.5" height="1" />
+      <rect class="stroke cls-4" x="1" y="1" width="62" height="8" />
+      <g class="building building--side">
+        <rect class="stroke" x="42" y="33" width="13" height="30" />
+        <path
+          class="building-windows"
+          d="M51,37H46a1,1,0,0,0,0,2h5A1,1,0,0,0,51,37Z"
+        />
+        <path
+          class="building-windows"
+          d="M51,42H46a1,1,0,0,0,0,2h5A1,1,0,0,0,51,42Z"
+        />
+        <path
+          class="building-windows"
+          d="M51,47H46a1,1,0,0,0,0,2h5A1,1,0,0,0,51,47Z"
+        />
+        <path
+          class="building-windows"
+          d="M51,52H46a1,1,0,0,0,0,2h5A1,1,0,0,0,51,52Z"
+        />
+      </g>
+      <g class="building building--side">
+        <rect class="stroke" x="9" y="33" width="13" height="30" />
+        <path
+          class="building-windows"
+          d="M13,39h5a1,1,0,0,0,0-2H13A1,1,0,0,0,13,39Z"
+        />
+        <path
+          class="building-windows"
+          d="M13,44h5a1,1,0,0,0,0-2H13A1,1,0,0,0,13,44Z"
+        />
+        <path
+          class="building-windows"
+          d="M13,49h5a1,1,0,0,0,0-2H13A1,1,0,0,0,13,49Z"
+        />
+        <path
+          class="building-windows"
+          d="M13,54h5a1,1,0,0,0,0-2H13A1,1,0,0,0,13,54Z"
+        />
+      </g>
+      <g class="building">
+        <rect class="stroke cls-5" x="24" y="19" width="16" height="4" />
+        <rect class="stroke" x="22" y="23" width="20" height="30" />
+        <path
+          class="building-windows"
+          d="M33,30V28a1,1,0,0,0-2,0v2A1,1,0,0,0,33,30Z"
+        />
+        <path
+          class="building-windows"
+          d="M38,30V28a1,1,0,0,0-2,0v2A1,1,0,0,0,38,30Z"
+        />
+        <path
+          class="building-windows"
+          d="M28,30V28a1,1,0,0,0-2,0v2A1,1,0,0,0,28,30Z"
+        />
+        <path
+          class="building-windows"
+          d="M33,37V35a1,1,0,0,0-2,0v2A1,1,0,0,0,33,37Z"
+        />
+        <path
+          class="building-windows"
+          d="M38,37V35a1,1,0,0,0-2,0v2A1,1,0,0,0,38,37Z"
+        />
+        <path
+          class="building-windows"
+          d="M28,37V35a1,1,0,0,0-2,0v2A1,1,0,0,0,28,37Z"
+        />
+      </g>
+      <rect class="cls-8" x="9.25" y="61" width="13" height="2" />
+      <rect class="cls-8" x="41.75" y="61" width="13" height="2" />
+      <g class="gear">
+        <path
+          class="stroke gear__top"
+          d="M50.1552 57.2183V57.2608V63.2183V63.2608H45.8252C45.4697 64.8743 44.8297 66.4116 43.9352 67.8008L47.0052 70.8708L42.7652 75.1108L39.6952 72.0408C38.3061 72.9353 36.7687 73.5752 35.1552 73.9308V78.2608H29.1552V73.9308C27.5417 73.5752 26.0044 72.9353 24.6152 72.0408L21.5452 75.1108L17.3052 70.8708L20.3752 67.8008C19.4808 66.4116 18.8408 64.8743 18.4852 63.2608H14.1552V63.2183V57.2608V57.2183H18.4852C18.8408 55.6048 19.4808 54.0674 20.3752 52.6783L17.3052 49.6083L21.5452 45.3683L24.6152 48.4383C26.0044 47.5438 27.5417 46.9038 29.1552 46.5483V42.2183H35.1552V46.5483C36.7687 46.9038 38.3061 47.5438 39.6952 48.4383L42.7652 45.3683L47.0052 49.6083L43.9352 52.6783C44.8297 54.0674 45.4697 55.6048 45.8252 57.2183H50.1552Z"
+        />
+        <g class="gear__code">
+          <path
+            d="M26.29,52.29l-3,3a1,1,0,0,0,0,1.42l3,3a1,1,0,0,0,1.42-1.42L25.41,56l2.3-2.29A1,1,0,0,0,26.29,52.29Z"
+          />
+          <path
+            d="M37.71,52.29a1,1,0,0,0-1.42,1.42L38.59,56l-2.3,2.29A1,1,0,0,0,37,60c.55,0,.43,0,3.71-3.29a1,1,0,0,0,0-1.42Z"
+          />
+          <path
+            d="M32.05,52.68l-2,6A1,1,0,0,0,31,60c1,0,.83-.32,3-6.68A1,1,0,0,0,32.05,52.68Z"
+          />
+        </g>
+      </g>
+      <path class="stroke button" d="M6,4A1,1,0,0,0,6,6,1,1,0,0,0,6,4Z" />
+      <path class="stroke button" d="M11,4a1,1,0,0,0,0,2A1,1,0,0,0,11,4Z" />
+      <path class="stroke button" d="M16,4a1,1,0,0,0,0,2A1,1,0,0,0,16,4Z" />
+      <rect
+        class="bottom-line"
+        x="0"
+        y="62"
+        width="62"
+        height="2"
+        rx="1"
+        ry="1"
+      />
+    </g>
+  </svg>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  name: 'SvgTechCompanies',
+});
+</script>
+
+<style lang="scss" scoped>
+.svg {
+  width: 100%;
+  height: 100%;
+}
+.stroke {
+  stroke-width: 0.5;
+  stroke-opacity: 1;
+  stroke: var(--stroke);
+  color: var(--stroke);
+}
+.cls-1 {
+  fill: var(--secondary);
+}
+.cls-2 {
+  fill: #eee;
+}
+.cls-4 {
+  fill: var(--secondary);
+}
+.cls-5 {
+  fill: var(--primary-dark);
+}
+.cls-8 {
+  fill: var(--tertiary-dark);
+}
+.gear {
+  &__top {
+    transform-box: fill-box;
+    transform-origin: center;
+    fill: var(--primary-dark);
+    animation: rotate 5s infinite linear;
+  }
+  &__code {
+    stroke: var(--primary-dark);
+    stroke-width: 1.5;
+    fill: var(--secondary);
+  }
+}
+.cls-10 {
+  fill: var(--primary-dark);
+}
+.button {
+  fill: var(--tertiary);
+}
+.bottom-line {
+  fill: var(--stroke);
+}
+.building {
+  fill: var(--primary-light);
+  animation-name: stretch;
+  animation-duration: 1s;
+  animation-iteration-count: infinite;
+  animation-direction: alternate-reverse;
+  animation-timing-function: linear;
+  &--side {
+    animation-delay: 1s;
+    fill: var(--tertiary);
+  }
+}
+.building-windows {
+  fill: var(--primary-dark);
+}
+
+@keyframes rotate {
+  from {
+    transform: rotate(0);
+  }
+  to {
+    transform: rotate(359deg);
+  }
+}
+
+@keyframes stretch {
+  from {
+    transform: translate3d(0, 0, 0);
+  }
+  to {
+    transform: translate3d(0, 5%, 0);
+  }
+}
+</style>
