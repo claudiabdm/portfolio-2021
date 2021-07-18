fa936f0f93deccbfd9ae366e940dac934dde7cfa
claudiabdm <claudiabdm@hotmail.es>
2021-07-18 22:57:23 +0200

    Add my profile component
    Signed-off-by: claudiabdm <claudiabdm@hotmail.es>

---
+++ components/MyProfile.vue
@@ -0,0 +1,114 @@
+<template>
+  <div class="profile">
+    <div class="profile__wrapper profile__wrapper--round">
+      <MyImage
+        ref="profileImg"
+        class="profile__img"
+        :blok="{ image: blok.profileImg, width: 300, height: 300 }"
+        border-radius="50%"
+      />
+    </div>
+    <div class="profile__wrapper">
+      <ul ref="profileLinks" class="profile__links">
+        <li v-for="link in links" :key="link.id" class="profile__link">
+          <a class="profile__a" :href="link.url" target="_blank" rel="noopener">
+            <svg class="profile__svg">
+              <use :href="iconPath(link.id)" />
+            </svg>
+          </a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import MyImage from '@/components/MyImage.vue';
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+
+if (process.client) {
+  gsap.registerPlugin(ScrollTrigger);
+}
+
+export default Vue.extend({
+  name: 'MyProfile',
+  components: { MyImage },
+  props: {
+    blok: {
+      type: Object,
+      default: () => {},
+    },
+  },
+  computed: {
+    links() {
+      return [
+        { ...this.blok.linkedin, id: 'linkedin' },
+        { ...this.blok.github, id: 'github' },
+        { ...this.blok.codepen, id: 'codepen' },
+      ];
+    },
+  },
+  mounted() {
+    const profile = this.$refs.profileImg as Vue;
+    const links = this.$refs.profileLinks as Element;
+    this.$elevateAnimation(profile.$el);
+    this.$elevateAnimation(links);
+  },
+  methods: {
+    iconPath(icon: string): string {
+      return require('@/assets/sprite.svg') + '#' + icon;
+    },
+  },
+});
+</script>
+
+<style scoped lang="scss">
+@use '~/assets/styles/global/variables' as *;
+@use '~/assets/styles/mixins/mixins' as *;
+.profile {
+  padding-left: rem(20px);
+  &__wrapper {
+    @include size(100%, 100%);
+    background-color: var(--shadow);
+    border-radius: $border-radius;
+    &--round {
+      border-radius: 50%;
+    }
+  }
+  &__img {
+    @include size(rem(200px), rem(200px));
+    border-radius: 50%;
+    border: rem(10px) solid var(--secondary);
+    box-shadow: 0 0 0 rem(2px) var(--stroke);
+    @media screen and (min-width: 1024px) {
+      @include size(rem(250px), rem(250px));
+    }
+  }
+  &__links {
+    @include flex(center, space-around);
+    @include size(100%, 100%);
+    margin-top: rem(25px);
+    background-color: var(--tertiary);
+    border-radius: $border-radius;
+    border: $border;
+  }
+  &__a {
+    @include flex(center, center);
+  }
+  &__link {
+    margin: rem(10px) 0;
+  }
+  &__svg {
+    @include size(rem(32px), rem(32px));
+    color: var(--secondary);
+    stroke: var(--stroke);
+    stroke-width: 10;
+    transition: color 0.15s ease-in-out;
+    &:hover {
+      color: var(--primary-light);
+    }
+  }
+}
+</style>
