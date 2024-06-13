651f10934de3d6ae23e538d8f47a0f7ed78ce4b3
claudiabdm <44007726+claudiabdm@users.noreply.github.com>
2024-06-13 18:24:22 +0200

    Move anchor

--- components/MyLink.vue
+++ components/MyLink.vue
@@ -20,27 +20,3 @@ withDefaults(defineProps<{ link: string, external?: boolean }>(), {
     </TextWithArrow>
   </NuxtLinkLocale>
 </template>
-
-<style lang="scss">
-a {
-  color: var(--primary);
-  background-color: transparent;
-  font-weight: 700;
-  font-family: var(--font-family-secondary);
-  line-height: 1.5;
-  text-decoration: underline;
-  text-decoration-color: var(--tertiary);
-  text-decoration-skip-ink: none;
-  text-decoration-thickness: 2px;
-  text-underline-offset: 5px;
-  transition: text-decoration-color 0.15s ease-out;
-
-  &:hover {
-    text-decoration-color: var(--secondary);
-  }
-
-  &:visited {
-    color: var(--primary);
-  }
-}
-</style>
