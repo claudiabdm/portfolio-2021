c69a803c35e7100788b5c6733aa4069b7f58f30b
claudiabdm <44007726+claudiabdm@users.noreply.github.com>
2024-06-14 15:45:13 +0200

    Add name to path

--- composables/useTranslateSlug.ts
+++ composables/useTranslateSlug.ts
@@ -4,6 +4,7 @@ export function useTranslateSlug() {
     const { t } = useI18n();
     return function translatePath(slug: string) {
         return {
+            name: 'slug',
             params: {
                 slug: t(slug + 'Slug') 
             }
